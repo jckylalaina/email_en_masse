99768cc3223f47af0739682460e7edd0dff30fe1
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-01-25 14:07:19 +0300

    modification of data class

--- lib/scarpper.rb
+++ lib/scarpper.rb
@@ -39,7 +39,8 @@ class Mail
 
 end
 class Data
-  @tabhash = Mail.getting_the_townhall
+  attr_accessor :tabhash
+  @tabhash = Mail.new.getting_the_townhall
   def save_as_JSON
   File.open("db/scrapper.json","w") do |f|
     f.write(JSON.pretty_generate(@tabhash))
